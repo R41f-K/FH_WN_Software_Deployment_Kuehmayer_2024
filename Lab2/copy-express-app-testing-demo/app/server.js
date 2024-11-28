d81f6d76c8456ef290bca7ad24178698ecf30971
R41f_K <ralf.kuehmayer@gmail.com>
2024-11-28 11:51:44 +0100

    adding azure functions as app

--- Lab2/copy-express-app-testing-demo/app/server.js
+++ Lab2/copy-express-app-testing-demo/app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
 
-const app = express();
+const app = require('@azure/funczions');;
 
 // public assets
 app.use(express.static(path.join(__dirname, 'public')));
